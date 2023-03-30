a96a664a60ee70095f485d4c662e03675be0e0b6
Andrew Pham <minhanh.vietpham@gmail.com>
2023-03-29 21:53:42 -0500

    PC: working on Compare

--- client/src/views/DetailPage.jsx
+++ client/src/views/DetailPage.jsx
@@ -6,6 +6,10 @@ import axios from "axios";
 import "./css/DetailPage.css";
 import Navbar from "../components/Navbar";
 
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
+import BedIcon from "@mui/icons-material/Bed";
+import SquareFootIcon from "@mui/icons-material/SquareFoot";
+
 const DetailPage = () => {
 	const [propertyDetail, setPropertyDetail] = useState();
 	const [areaZipcode, setAreaZipcode] = useState();
@@ -29,9 +33,9 @@ const DetailPage = () => {
 			.get(`http://localhost:8080/api/areas/zipcode/${areaZipcode}`)
 			.then((res) => (console.log("areaInfo", res.data), setAreaInfo(res.data)))
 			.catch((err) => console.log("err from areaInfo", err));
-	}, []);
+	}, [areaInfo]);
 	return (
-		<div>
+		<div className="detailPage">
 			<div className="nav">
 				<h3>BPlace</h3>
 				<Navbar className="navbar" />
@@ -41,11 +45,76 @@ const DetailPage = () => {
 				/>
 			</div>
 			<SearchTools />
-			<div className="container">
-				<div className="images">
-					<img src="" alt="" />
+			{propertyDetail && (
+				<div className="detailPage_container">
+					<div className="images">
+						<div className="bigImage">
+							<img src={propertyDetail.imageUrls[0]} />
+						</div>
+						<div className="sideImages">
+							<img src={propertyDetail.imageUrls[1]} />
+							<img src={propertyDetail.imageUrls[2]} />
+						</div>
+					</div>
+					<div className="address_detail">
+						<h3>{propertyDetail.address}</h3>
+						<p>{propertyDetail.city}</p>
+						<p>
+							{propertyDetail.state}, {propertyDetail.zipcode}
+						</p>
+					</div>
+					<div className="primary_detail">
+						<div className="material_detail">
+							<div className="price">
+								<LocalOfferIcon />
+								<h3>${propertyDetail.price.toLocaleString()}</h3>
+							</div>
+							<div className="bedroom">
+								<BedIcon />
+								<p>{propertyDetail.bedrooms}</p>
+							</div>
+							<div className="sqft">
+								<SquareFootIcon />
+								<p>{propertyDetail.sqft} ft&sup2;</p>
+							</div>
+						</div>
+						<div className="area_detail">
+							{areaInfo && (
+								<div className="area">
+									<h4>
+										Population: <span>{areaInfo.population}</span>
+									</h4>
+									<h4>
+										Livability: <span>{areaInfo.livability}</span>
+									</h4>
+									<h4>
+										Amenities: <span>{areaInfo.amenities}</span>
+									</h4>
+									<h4>
+										Cost Of Living: <span>{areaInfo.costOfLiving}</span>
+									</h4>
+									<h4>
+										Crime: <span>{areaInfo.crime}</span>
+									</h4>
+									<h4>
+										Employment: <span>{areaInfo.employment}</span>
+									</h4>
+									<h4>
+										Housing: <span>{areaInfo.housing}</span>
+									</h4>
+									<h4>
+										School: <span>{areaInfo.school}</span>
+									</h4>
+								</div>
+							)}
+						</div>
+					</div>
+					<div className="description">
+						<h3>Description</h3>
+						<p>{propertyDetail.description}</p>
+					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 };
